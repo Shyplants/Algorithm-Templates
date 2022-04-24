8afb831738f751af384875598347c6351b57dd7e
Shyplants <wlsdn1004100@naver.com>
2022-04-24 20:46:00 +0900

    UPD

--- math/lcm.cpp
+++ math/lcm.cpp
@@ -6,9 +6,13 @@ int gcd(int a, int b) {
   return b == 0 ? a : gcd(b, a % b);
 }
 
+int lcm(int a, int b) {
+  return a/gcd(a, b)*b;
+}
+
 int main()
 {
   int a = 6, b = 14;
-  cout << gcd(a, b); // b must not be zero.
+  cout << lcm(a, b); // b must not be zero.
   return 0;
 }
